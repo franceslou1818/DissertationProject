a7c317866a485c1d23d66f34098caf5ba525077b
Frances Ramirez <franceslou_1818@yahoo.com>
2018-10-24 19:40:50 +0100

    saloon in officers deckhouse created

---
+++ VirtualHeritageApp/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void OnMouseDown() {
+		Debug.Log ("OnMouseDown() in ChangeScene.cs was called!");
+		SceneManager.LoadScene("FrontDeckhouse");
+	}
+
+//	public void GotoScene(string sceneName) {
+//		print ("test print");
+//		Debug.Log ("My go to scene method was called!");
+//		SceneManager.LoadScene (sceneName);
+//	}
+}
