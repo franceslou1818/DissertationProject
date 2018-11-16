5a6b9a75f1fee7924c991542e1f545aff59096e9
Frances Ramirez <franceslou_1818@yahoo.com>
2018-11-16 10:06:19 +0000

    taking a step back and do more tutorials

---
+++ VirtualHeritageApp/Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HighlightObject : MonoBehaviour {
+
+	private Renderer myRenderer;
+
+	public Material inactiveMaterial;
+	public Material gazedAtMaterial;
+
+	void Start() {
+		myRenderer = GetComponent<Renderer>();
+		SetGazedAt(false);
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+
+		print ("highlight");
+		if (inactiveMaterial != null && gazedAtMaterial != null) {
+			myRenderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+			return;
+		}
+	}
+
+
+}
